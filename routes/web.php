c8f725a8afd8434288b19dcce218b9c1c062fcde
Carlos Sosa <carlos.alberto.sosa.perera@gmail.com>
2023-07-12 19:06:10 -0600

    adding tall stack

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->to('/dashboard');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        app()->setLocale('es');
+        return view('dashboard');
+    })->name('dashboard');
 });
