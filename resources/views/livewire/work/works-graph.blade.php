5a57e9cb7f12541ed9b654d8f8090b4bcc9a0e86
Carlos Sosa <carlos.alberto.sosa.perera@gmail.com>
2023-10-31 13:29:17 -0600

    removing chartjs js file from vite config

--- resources/views/livewire/work/works-graph.blade.php
+++ resources/views/livewire/work/works-graph.blade.php
@@ -1,8 +1,8 @@
 <div class="w-full">
-    <a href="{{route('work.index')}}" class="w-full flex flex-col gap-y-3">
-        <span class="text-gray-800 dark:text-gray-200 text-2xl font-bold">Solicitudes</span>
+    <span class="text-gray-800 dark:text-gray-200 text-2xl font-bold">Solicitudes</span>
+    <div class="max-w-2xl mx-auto">
         <canvas id="chart"></canvas>
-    </a>
+    </div>
     @push('scripts')
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.0/chart.umd.js" integrity="sha512-6HrPqAvK+lZElIZ4mZ64fyxIBTsaX5zAFZg2V/2WT+iKPrFzTzvx6QAsLW2OaLwobhMYBog/+bvmIEEGXi0p1w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script>
