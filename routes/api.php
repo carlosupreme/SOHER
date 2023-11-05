0f0e5f0e20a64f527800b6da1037c9a7ac8427b6
Carlos Sosa <carlos.alberto.sosa.perera@gmail.com>
2023-11-04 22:50:19 -0600

    removing auth from route (temporaly)

--- routes/api.php
+++ routes/api.php
@@ -23,8 +23,8 @@ use Illuminate\Support\Facades\Route;
 Route::post('auth/register', [AuthApiController::class, 'create']);
 Route::post('auth/login', [AuthApiController::class, 'login']);
 
-Route::middleware(['auth:sanctum'])->group(function () {
+//Route::middleware(['auth:sanctum'])->group(function () {
     Route::apiResource('/works', WorkApiController::class)->names('work');
     Route::get('auth/logout', [AuthApiController::class, 'logout']);
-});
+//});
 
